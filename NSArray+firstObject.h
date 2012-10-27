41fdd38f2f1ccf5c7a26e54862c0669d2704a6d0
Web <Web@Michaels-MacBook-Air.local>
2012-10-27 03:39:07 -0400

    Initial commit

---
+++ NSArray+firstObject.h
@@ -0,0 +1,8 @@
+//  NSArray+firstObject.h
+//
+//  NO COPYRIGHT AND ABSOLUTELY NO WARRANTY
+//
+//  DO WHAT EVER YOU WANT WITH THIS CATEGORY
+@interface NSArray (firstObject)
+- (id) firstObject;
+@end
---
+++ NSArray+firstObject.m
@@ -0,0 +1,13 @@
+//  NSArray+firstObject.m
+//
+//  NO COPYRIGHT AND ABSOLUTELY NO WARRANTY
+//
+//  DO WHAT EVER YOU WANT WITH THIS CATEGORY
+#import "NSArray+firstObject.h"
+@implementation NSArray (firstObject)
+- (id) firstObject {
+    if (self.count > 0) {
+        return [self objectAtIndex:0];
+    }
+}
+@end
